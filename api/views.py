5bd82fdb154655c8036fc549f37980791d204484
A1ej00 <146979603+A1ej00@users.noreply.github.com>
2025-01-08 21:07:27 -0500

    actividad

--- api/views.py
+++ api/views.py
@@ -42,3 +42,45 @@ class LandingAPI(APIView):
 	        
         # Devuelve el id del objeto guardado
         return Response({"id": new_resource.key}, status=status.HTTP_201_CREATED)
+    
+class LandingAPIDetail(APIView):
+
+    name = 'Landing Detail API'
+
+    collection_name = 'coleccion'
+
+    def get(self, request, pk):
+
+        ref = db.reference(f'{self.collection_name}/{pk}')
+        document = ref.get()
+
+        if document:
+            return Response(document, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Documento no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, pk):
+        ref = db.reference(f'{self.collection_name}/{pk}')
+        document = ref.get()
+
+        if not document:
+            return Response({"error": "404 Not Found"}, status=status.HTTP_404_NOT_FOUND)
+
+        data = request.data
+        if 'email' not in data:
+            return Response({"error": "Faltan campos requeridos"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        ref.update(data)
+
+        return Response({"message": "Documento actualizado correctamente"}, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        ref = db.reference(f'{self.collection_name}/{pk}')
+        document = ref.get()
+
+        if document is not None:
+            ref.delete()
+            return Response({"message": "Documento eliminado correctamente"}, status=status.HTTP_204_NO_CONTENT)
+        else:
+            # Si no se encuentra el documento, retornamos error 404
+            return Response({"error": "Documento no encontrado"}, status=status.HTTP_404_NOT_FOUND)
